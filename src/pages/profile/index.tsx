e5d47a84e28059b8b20919c4014e17056c03fab0
Sit Naing Soe <sitnaingsoe740@gmail.com>
2026-01-09 11:41:05 +0630

    third commit

--- src/pages/profile/index.tsx
+++
@@ -1,9 +0,0 @@
-import {Box, Button, Typography} from "@mui/material";
-const profile = () => {
-    return (
-    <Box>
-    
-    <Typography sx={{m: 10, fontSize: "30px"}}>Sit Naing Soe</Typography>
-    </Box>
-    );
-    }
